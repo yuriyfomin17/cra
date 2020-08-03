cb881047f1ffaecf198f4c1957b0ce8b2abf76fd
yuriyfomin17 <fominyura2012@gmail.com>
2020-08-03 18:25:41 +0100

    Updates

---
+++ precache-manifest.814cab277348d4e5667e9846fb4735c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1681787c5bbfaae2c3608ba1c186ac62",
+    "url": "/cra/index.html"
+  },
+  {
+    "revision": "7ff8252a99d1b3741327",
+    "url": "/cra/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e2938574dc45edab7432",
+    "url": "/cra/static/js/2.ad2f2c54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cra/static/js/2.ad2f2c54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ff8252a99d1b3741327",
+    "url": "/cra/static/js/main.bf687cba.chunk.js"
+  },
+  {
+    "revision": "65aed08eb4484848b89e",
+    "url": "/cra/static/js/runtime-main.c31b0881.js"
+  }
+]);
